b37006cfea895427a6deeb49c7256be916f1c756
Jordan Pasker <jordan.pasker@mines.sdsmt.edu>
2018-02-17 12:52:55 -0700

    Changed kinematics, added wireless comms

--- remote_control/remote_control.ino
+++ remote_control/remote_control.ino
@@ -1,11 +1,68 @@
-//empty :(
-void setup()
-{
+#include <SoftwareSerial.h>
 
+SoftwareSerial xbee(2,3); //rx, tx
+
+int joy_1a = 0;
+int joy_1b = 0;
+
+int joy_2a = 0;
+int joy_2b = 0;
+
+int map_1a = 0;
+int map_1b = 0;
+
+int map_2a = 0;
+int map_2b = 0;
+
+void setup() {
+  pinMode(A0, INPUT);     // sets A0 to input
+  pinMode(A1, INPUT);     // sets A0 to input
+  pinMode(A2, INPUT);     // sets A0 to input
+  pinMode(A3, INPUT);     // sets A0 to input
+
+  Serial.begin(9600); //open serial port
+  Serial.println("Starting Send Code");
+  
+  xbee.begin(9600); //open serial port
+  xbee.println("SERIAL BEGIN");
 }
 
-void loop()
-{
- 
-}
+void loop() {
+  joy_1a = analogRead(A0);      // gets the joystick output for the vertical value 
+  joy_1b = analogRead(A1);
+  joy_2a = analogRead(A2);
+  joy_2b = analogRead(A3);
 
+  map_1a = map(joy_1a, 0, 1024, 0, 200);
+  map_1b = map(joy_1b, 0, 1024, 0, 200);
+  map_2a = map(joy_2a, 0, 1024, 0, 200);
+  map_2b = map(joy_2b, 0, 1024, 0, 200);
+  
+  xbee.write('a');
+  xbee.write(map_1a);
+  xbee.write('X');
+  Serial.print(map_1a);
+  Serial.print(", ");
+  delay(500);
+  
+  xbee.write('b');
+  xbee.write(map_1b);
+  xbee.write('X');
+  Serial.print(map_1b);
+  Serial.print(", ");
+  delay(500);
+  
+  xbee.write('c');
+  xbee.write(map_2a);
+  xbee.write('X');
+  Serial.print(map_2a);
+  Serial.print(", ");
+  delay(500);
+  
+  xbee.write('d');
+  xbee.write(map_2b);
+  xbee.write('X');
+  Serial.print(map_2b);
+  Serial.println();
+  delay(500);
+}
--- robot_drive/robot_drive.ino
+++ robot_drive/robot_drive.ino
@@ -1,192 +1,65 @@
-#include "RoboClaw.h"
-#include <SoftwareSerial.h>
-#include "MatrixMath.h" 
+//Recieving byte
+byte incomingByte = 0;
 
-RoboClaw roboclawOmni(&Serial1, 10000);
-RoboClaw roboclawMcCanum(&Serial2, 10000);
+//Storage for bytes for decoding
+byte incoming_command[2];
+int queue_len = 0;
 
-#define address 0x80
+//storage for values being sent [button / joy] [value]
+//97-100 are joystick values
+//101-108 are button values
+int current_vals[12][2];
 
-const float RADIUS_OMNI = 1.5;      // radius of the omni wheel
-const float RADIUS_MECANUM = 1.17;  // radius of the mecanum wheels
-const float L_O = 3;                // distance from the omni wheel to the geometric center of the chassis
-
-// Constant Matrix used in finding the xsi-sub-R
-const float m1[9] = { 0, (-(RADIUS_MECANUM / 2)), (RADIUS_MECANUM / 2),
-                      0, (RADIUS_MECANUM / 2), (RADIUS_MECANUM / 2),
-                      (-(RADIUS_OMNI / L_O)), (RADIUS_OMNI / (2 * L_O)), (-(RADIUS_OMNI / (2 * L_O))) };
-
-int vertical = A0;    // joystick's vertical output wires to the A0 pin.
-int horizontal = A1;  // joystick's horizontal output wires to the A1 pin.
-int verticalSensor = 0;   // holds the vertical analog output from joystick
-int horizontalSensor = 0; // holds the horizontal analog output from joystick
-
-void setup()
+int axis = 0;
+//Distinguishes from button / joystick commands
+void check_command()
 {
-  roboclawOmni.begin(38400);
-  roboclawMcCanum.begin(38400);
-
-  pinMode(vertical, INPUT);     // sets A0 to input
-  pinMode(horizontal, INPUT);   // sets A1 to input
-  Serial.begin(57600);
-
-  Matrix.Print(m1, 3, 3, "m1");   // display xsi_sub_R to serial monitor
-
-  Serial.println();
-
-  double phi_o = get_phi_o(25, 0);      // phi vector value for the rotational speed of the omni wheel
-  double phi_m1 = get_phi_m1(25, 60);   // phi vector value for the rotational speed of the upper left mecanum wheel
-  double phi_m2 = get_phi_m2(75, 80);   // phi vector value for the rotational speed of the upper right mecanum wheel
-
-  float velocity_vector[3];   // xsi_sub_r = constant matrix (m1) * phi vector
-
-  // calculate xsi_sub_r using the constant matrix, m1, and the phi vector
-  set_vel_vector(velocity_vector, phi_o, phi_m1, phi_m2);
-}
-
-void loop()
-{
-  verticalSensor = analogRead(vertical);      // gets the joystick output for the vertical value 
-  horizontalSensor = analogRead(horizontal);  // gets the joystick output for the horizontal value
-
-  // maps the forward/backward and left/right movement 
-  // with the correct ratio from the joystick to the motors
-  int fwd_bwd = map(verticalSensor, 0, 1022, -127, 127);        
-  int left_right = map(horizontalSensor, 0, 1022, -127, 127);
-
-  // phi vector components
-  double phi_o = get_phi_o(left_right, 0);
-  double phi_m1 = get_phi_m1(left_right, fwd_bwd);
-  double phi_m2 = get_phi_m2(left_right, fwd_bwd);
-
-  // determines direction of omni wheel
-  if (phi_o > 0)
-    roboclawOmni.ForwardM1(address, phi_o);
-  else roboclawOmni.BackwardM1(address, abs(phi_o));
-
-  // determines direction of upper left mecanum wheel
-  if (phi_m1 > 0)
-    roboclawMcCanum.BackwardM1(address, phi_m1);
-  else roboclawMcCanum.ForwardM1(address, abs(phi_m1));
-
-  // determines direction of upper right mecanum wheel
-  if (phi_m2 > 0)
-    roboclawMcCanum.BackwardM2(address, phi_m2);
-  else roboclawMcCanum.ForwardM2(address, abs(phi_m2));
-  
-// Previous attempt at movement. May still be useful
-/*
-  if (left_right > 10 || left_right < -10 && fwd_bwd > 10 || fwd_bwd < -10)
+  //incoming_command[0] is the encoded value being converted to decimal
+  if(incoming_command[0] <= 100)
   {
-    // 1st Quadrant
-    if (fwd_bwd > 0 && left_right > 0 && left_right > fwd_bwd)
-    {
-      roboclawMcCanum.ForwardM1(address, (MAX_SPEED * ratio(fwd_bwd, left_right)));
-      roboclawMcCanum.BackwardM2(address, MAX_SPEED);
-    }
-
-    // 2th Quadrant
-    if (fwd_bwd > 0 && left_right > 0 && left_right < fwd_bwd)
-    {
-      roboclawMcCanum.BackwardM1(address, (MAX_SPEED * ratio(fwd_bwd, left_right)));
-      roboclawMcCanum.BackwardM2(address, MAX_SPEED);
-    }
-    
-    // 3nd Quadrant
-    if (fwd_bwd > 0 && left_right < 0 && abs(left_right) < fwd_bwd)
-    {
-      roboclawMcCanum.BackwardM1(address, fwd_bwd);
-      roboclawMcCanum.BackwardM2(address, abs(left_right));
-    }
-
-    // 4th Quadrant
-    if (fwd_bwd > 0 && left_right < 0 && abs(left_right) > fwd_bwd)
-    {
-      roboclawMcCanum.BackwardM1(address, fwd_bwd);
-      roboclawMcCanum.ForwardM2(address, abs(left_right));
-    }
-    
-    // 5rd Quadrant
-    if (fwd_bwd < 0 && left_right < 0 && abs(left_right) > abs(fwd_bwd))
-    {
-      roboclawMcCanum.BackwardM1(address, abs(fwd_bwd));
-      roboclawMcCanum.ForwardM2(address, abs(left_right));
-      // roboclawOmni.BackwardM1(address, left_right);
-    }
-     
-    // 6th Quadrant
-    if (fwd_bwd < 0 && left_right < 0 && abs(left_right) < abs(fwd_bwd))
-    {
-      roboclawMcCanum.ForwardM1(address, abs(left_right));
-      roboclawMcCanum.ForwardM2(address, abs(fwd_bwd));
-    }  
-        
-    // 7th Quadrant
-    if (fwd_bwd < 0 && left_right > 0 && left_right < abs(fwd_bwd))
-    {
-      roboclawMcCanum.ForwardM1(address, left_right);
-      roboclawMcCanum.ForwardM2(address, abs(fwd_bwd));
-      //roboclawOmni.ForwardM1(address, abs(left_right));
-    }
-      
-    // 8th Quadrant
-    if (fwd_bwd < 0 && left_right > 0 && left_right > abs(fwd_bwd))
-    {
-      roboclawMcCanum.ForwardM1(address, left_right);
-      roboclawMcCanum.BackwardM2(address, abs(fwd_bwd));
-    }
+    axis = incoming_command[0];
+    current_vals[(axis - 97)][0] = axis;
+    current_vals[(axis - 97)][0] = map(incoming_command[1], 0, 200, -100, 100);
+    Serial.print("Joystick ");
+    Serial.print(incoming_command[0], HEX);
+    Serial.print(" ");
+    Serial.print(map(incoming_command[1], 0, 200, -100, 100));
+    Serial.println();
   }
-  // sit still
-  else
-  {
-      roboclawMcCanum.ForwardM1(address, 0);
-      roboclawMcCanum.BackwardM2(address, 0);
-      roboclawOmni.ForwardM1(address, 0);
-  }
-*/
-// print functions to help debug
-//  Serial.print("Horizontal: ");
-//  Serial.println(left_right);
-//  Serial.print("Vertical: ");
-//  Serial.println(fwd_bwd);
-//  Serial.println(verticalSensor);
-//  Serial.println(horizontalSensor);
+}
+
+//houses the code for driving the robot
+void drive()
+{
   
-  delay(100);
+}
+void setup() {
+  Serial1.begin(9600);
+  Serial.begin(9600);
+
+  Serial.println("Starting Recieve Code");
 }
 
-// calculates the rotational speed of the omni wheel based on a desired direction, x,
-// and a desired angle, theta
- double get_phi_o(int x, int theta)
-{
-  double phi_o = (-((x + (L_O * theta)) / RADIUS_OMNI));
-  return phi_o;
-}
+void loop() {
+  if (Serial1.available() > 0)
+  {
+    incomingByte = Serial1.read();
+    if (incomingByte == 0x58)
+    {
+      Serial.println();
+      check_command();
+      drive()
+      queue_len = 0;
+    } 
+    else 
+    {
+      incoming_command[queue_len] = incomingByte;
+      queue_len++;
+      Serial.print(incomingByte, HEX);
+      Serial.print(" ");
+    }
+   
+    //Serial.println(incomingByte, HEX);
+  }
 
-// calculates the rotational speed of the northwest mecanum wheel based on a 
-// desired direction in x and y
-double get_phi_m1(int x, int y)
-{
-  double phi_m1 = ((x + y) / RADIUS_MECANUM);
-  return phi_m1;
-}
-
-// calculates the rotational speed of the northwest mecanum wheel based on a 
-// desired direction in x and y
-double get_phi_m2(int x, int y)
-{
-  double phi_m2 = (((-x) + y) / RADIUS_MECANUM);
-  return phi_m2;
-}
-
-// caluclates xsi_sub_R using the phi vector and constant matrix
-void set_vel_vector(float* velocity, float phi_o, float phi_m1, float phi_m2)
-{  
-  float phi_velocity[3] = { phi_o,
-                            phi_m1,
-                            phi_m2 };
-
-  Matrix.Multiply(m1, phi_velocity, 3, 3, 1, velocity);                
-  Matrix.Print(velocity, 3, 1, "velocity");
-  Serial.println();
 }

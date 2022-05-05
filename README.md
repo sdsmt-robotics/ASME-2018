a4fee87a81cc2f651d99245b3273c1cda9098148
Esjayar <samuel.ryckman@mines.sdsmt.edu>
2022-05-04 21:04:00 -0600

    Update orange defense bot to use new xBee controller lib

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 The repository for the SD Mines Robotics Team ASME Robot Design Competition robot and controller code.
 
-Code for the robot will be in the robot_drive folders, and code for the controller will be in remote_control.
+Code for each robot will be in the robot_drive folders.
 
 ## Libraries used
 
@@ -11,8 +11,11 @@ We are using the following libraries in this code
 * Ionmotion roboclaw libraries for Arduino
 * http://www.ionmc.com/downloads
 
-* Pololu Maestro libraries for Arduino
-* https://github.com/pololu/maestro-arduino
+* Our custom xBee controller library  
+* https://github.com/sdsmt-robotics/Controller/tree/master/Code/receive
+
+* Our simpley L298N motor controller library  
+* https://github.com/sdsmt-robotics/L298N
 
 ## Additional notes:
 
--- robot_drive_defence_orange/robot_drive_defence_orange.ino
+++ robot_drive_defence_orange/robot_drive_defence_orange.ino
@@ -1,13 +1,13 @@
 //#include "RoboClaw.h"
+#include "Controller.h"
 #include "L289N.h"
 
-#define address 0x80
+#define KICK_LEN 250 // Length of a kick in ms
 
 int solenoid_pin = 44; //pin the h-bridge/solenoid is on
-const int JOY_THRESH = 15;  //dead zone for the joystick
 
-//RoboClaw FrontMotors(&Serial2, 10000); //right motors
-//RoboClaw BackMotors(&Serial3, 10000);
+//Create the communications object. Use Serial for the communications.
+Controller controller(Serial2);
 
 //Set up motor controllers
 L289N frontLeftMotor(2, 3, 4, true); //setup a motor object with pins 2 and 3 controlling direction and 4 controlling speed
@@ -15,104 +15,28 @@ L289N backLeftMotor(5, 6, 7, true);
 L289N frontRightMotor(8, 9, 10);
 L289N backRightMotor(11, 12, 13);
 
-
-//Recieving byte
-byte incomingByte = 0;
-
-//Storage for bytes for decoding
-byte incoming_command[2];
-int queue_len = 0;
-
-//storage for values being sent [button / joy] [value]
-//97-100 are joystick values
-//101-108 are button values
-int current_vals[12];
-
-long int old_time = 0;
-
-int axis = 0;
-
-//Distinguishes from button / joystick commands
-void check_command()
-{
-  //current_vals[4] = 0; //reset solenoid status
-  
-  //incoming_command[0] is the encoded value being converted to decimal
-  if(incoming_command[0] >= 97 && incoming_command[0] <= 100) //axis a-d
-  {
-    axis = incoming_command[0];
-    current_vals[(axis - 97)] = map(int(incoming_command[1]), 0, 200, -127, 127);
-    /*Serial.print("Joystick ");
-    Serial.print(incoming_command[0], HEX);
-    Serial.print(" ");
-    Serial.print(map(incoming_command[1], 0, 200, -127, 127));
-    Serial.println();*/
-  }
-  else if (incoming_command[0] == 'G') //button 3 on controller
-  {
-    current_vals[4] = 1; //fire solenoid
-  }
-
-  //Serial.println(current_vals[4]);
-}
-
-//houses the code for driving the robot
-void drive_bot()
-{
-  //kicker
-    if (current_vals[4] == 1)
-    {
-      old_time = millis();
-      analogWrite(solenoid_pin, 200); //fire solenoid
-    }
-    
-    //Print Joystick vals
-    Serial.println(current_vals[0]);
-    Serial.println(current_vals[1]);
-    Serial.println();
-
-    //Drive motors
-    if(abs(current_vals[1]) > JOY_THRESH) //left motors
-    {
-      frontLeftMotor.setSpeedDirection(current_vals[1] * 2);
-      backLeftMotor.setSpeedDirection(current_vals[1] * 2);
-     }
-    else 
-    {
-      frontLeftMotor.setSpeedDirection(0);
-      backLeftMotor.setSpeedDirection(0);
-    }
-    
-    if(abs(current_vals[0]) > JOY_THRESH) //right motors
-    {
-      frontRightMotor.setSpeedDirection(current_vals[0] * 2);
-      backRightMotor.setSpeedDirection(current_vals[0] * 2);
-    }
-    else
-    {
-      frontRightMotor.setSpeedDirection(0);
-      backRightMotor.setSpeedDirection(0);
-    }
-}
-
 void setup() {
-  pinMode(solenoid_pin, OUTPUT); //pwm pin for h-bridge/solenoid
+  Serial.begin(115200);
   
-  Serial1.begin(9600);
-  Serial.begin(9600);
+  //initialize the receiver
+  controller.init();
+  Serial.println("Waiting for connection...");
+  while (!controller.connected()) { delay(10); }
+  Serial.println("Connected!");
 
-  //initialize the motorsset
+  //set a deadzone for the joysticks
+  controller.setJoyDeadzone(0.08);
+
+  Serial.println("Initializing motors...");
+  //initialize the motors
   frontLeftMotor.init();
   backLeftMotor.init();
   frontRightMotor.init();
   frontLeftMotor.init();
 
-  //init comms with motor controllers
-  //FrontMotors.begin(38400);
-  //BackMotors.begin(38400);
-
-  Serial.println("Starting Recieve Code");
-
+  // Solenoid init
+  pinMode(solenoid_pin, OUTPUT); //pwm pin for h-bridge/solenoid
+  // Do some kiks
   analogWrite(solenoid_pin, 200);
   delay(500);
   analogWrite(solenoid_pin, 0);
@@ -120,39 +44,45 @@ void setup() {
   analogWrite(solenoid_pin, 200);
   delay(500);
   analogWrite(solenoid_pin, 0);
+  Serial.println("Done!");
+
+  Serial.println("Starting main loop.");
 }
 
 void loop() {
-  if (millis() - old_time > 100)
-  {
-    analogWrite(solenoid_pin, 0);
-    current_vals[4] = 0;
-  }
+  static bool disconnected = false;
   
-  if (Serial1.available() > 0)
-  {
-    incomingByte = Serial1.read();
-    if (incomingByte == 0x58)
-    {
-      //Serial.print("RECEIVED ");
-      //Serial.print(incoming_command[0]);
-      //Serial.println();
-      check_command();
-      drive_bot();
-      queue_len = 0;
-    } 
-    else 
-    {
-      if(queue_len >= 2)
-      {
-        queue_len = 0;
-      }
-      incoming_command[queue_len] = incomingByte;
-      queue_len++;
-      //Serial.print(incomingByte, HEX);
-      //Serial.print(" ");
+  if (controller.connected()) {
+    if (disconnected) {
+      Serial.println("Reconnected!");
+      disconnected = false;
     }
-    //Serial.println(incomingByte, HEX);
+    
+    // Kicker
+    if (controller.button(DOWN)) {
+      analogWrite(solenoid_pin, 200); // fire solenoid
+    } else {
+      analogWrite(solenoid_pin, 0); // Retract solenoid
+    }
+    
+    // Driving
+    int leftSpeed = controller.joystick(LEFT, Y) * 255;
+    int rightSpeed = controller.joystick(RIGHT, Y) * 255;
+    frontLeftMotor.setSpeedDirection(leftSpeed);
+    backLeftMotor.setSpeedDirection(leftSpeed);
+    frontRightMotor.setSpeedDirection(rightSpeed);
+    backRightMotor.setSpeedDirection(rightSpeed);
+    
+  } else {
+    if (!disconnected) {
+      Serial.println("Disconnected! Waiting for reconnect...");
+      disconnected = true;
+      
+      analogWrite(solenoid_pin, 0); // Retract solenoid
+      frontLeftMotor.setSpeedDirection(0);
+      backLeftMotor.setSpeedDirection(0);
+      frontRightMotor.setSpeedDirection(0);
+      backRightMotor.setSpeedDirection(0);
+    }
   }
-
 }
